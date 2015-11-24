2c6bfebae0e509a7992e6e2454981a7253a4b77f
William Mura <william57m@gmail.com>
2015-11-24 15:40:02 +0100

    Update to work without rails

--- lib/osx_strings_file_parser.rb
+++ lib/osx_strings_file_parser.rb
@@ -1,68 +1,85 @@
 require 'active_support'
 require 'iconv'
 
+class Const
+
+  class << self;
+    attr_accessor :REGEX_COMMENT_MULTI
+    attr_accessor :REGEX_COMMENT_SINGLE
+    attr_accessor :REGEX_LINES
+    attr_accessor :REGEX_QUOTED
+    attr_accessor :REGEX_KEY_VALUE
+  end
+
+  self.REGEX_COMMENT_MULTI     = /\/\*(.*?)\*\//
+  self.REGEX_COMMENT_SINGLE    = /\/\/[^\n]*/
+  self.REGEX_LINES             = /\r\n|\r|\n|\u0085|\u2028|\u2029/
+  self.REGEX_QUOTED            = /\"((\\\"|[^\"])+)\"/
+  self.REGEX_KEY_VALUE         = /#{Const.REGEX_QUOTED}\s*=\s*#{Const.REGEX_QUOTED}/
+end
+
 class OsxStringsFileParser
-  class FauxEncodingHandler
-    def self.convert( data )
-      data
+
+  class KeyValuePair
+
+    attr_accessor :key
+    attr_accessor :value
+    attr_accessor :comments
+
+    def initialize(key, value, comments)
+        @key = key
+        @value = value
+        @comments = comments
     end
-  end
-  
-  class KeyValuePair < Struct.new(:key, :value, :comments)
+
     def attributes
-      { :text => value, :data => { 'key' => key, 'comments' => comments }}
+      { 'text' => @value, 'data' => { 'key' => @key, 'comments' => @comments }}
     end
+
   end
-  
-  REGEX_COMMENT_MULTI  = /\/\*(.*?)\*\//  
-  REGEX_COMMENT_SINGLE = /\/\/[^\n]*/
-  REGEX_LINES = /\r\n|\r|\n|\u0085|\u2028|\u2029/  
-  REGEX_QUOTED = /\"((\\\"|[^\"])+)\"/
-  REGEX_KEY_VALUE = /#{REGEX_QUOTED}\s*=\s*#{REGEX_QUOTED}/
-  
-  class_inheritable_accessor :encoding_handler
-  self.encoding_handler = FauxEncodingHandler
-  
-  def initialize( data )
-    @data = encoding_handler.convert(data)
-    @lines ||= @data.chars.split(REGEX_LINES)
+
+  def initialize(data)
+    @data = data
+    @lines ||= @data.split(Const.REGEX_LINES)
     @idx = -1
     @line = nil
     @pairs = []
     @comments = []
-    
+
     while next_line
-      if comment = @line.match(REGEX_COMMENT_MULTI)
+      if comment = @line.match(Const.REGEX_COMMENT_MULTI)
         @comments << comment[1]
-        unless (@remainder = @line.gsub(REGEX_COMMENT_MULTI, '').strip) == ''
-          if pair = @remainder.match(REGEX_KEY_VALUE)
+        unless (@remainder = @line.gsub(Const.REGEX_COMMENT_MULTI, '').strip) == ''
+          if pair = @remainder.match(Const.REGEX_KEY_VALUE)
             @pairs << KeyValuePair.new(pair[1], pair[3], @comments.dup)
             @comments = []
           end
         end
-      elsif pair = @line.match(REGEX_KEY_VALUE)
+      elsif pair = @line.match(Const.REGEX_KEY_VALUE)
         @pairs << KeyValuePair.new(pair[1], pair[3], @comments.dup)
         @comments = []
       end
     end
   end
-  
+
   def next_line
     @line = @lines[@idx += 1]
   end
-  
+
   def lines
     @pairs
   end
-  
+
   def to_hash
     return @hash if @hash
-    @hash = returning( {} ) do |hash|
-      lines.each { |key_value| hash[key_value.key] = key_value.value }
+    @hash = Hash.new
+    for key_value in lines
+        @hash[key_value.key] = key_value.value
     end
+    return @hash
   end
-  
+
   def self.parse( data )
-    new( data ).to_hash
+    new(data).to_hash
   end
 end
